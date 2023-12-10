6fd66cff12a6105564d2097d070e4d378838809c
Petr Zahradnik <junior@zahradnik.cz>
2023-12-10 20:36:58 +0100

    Update ResnetBlock1D to match ResnetBlock2D

--- src/diffusers/models/resnet.py
+++ src/diffusers/models/resnet.py
@@ -614,63 +614,122 @@ class ResnetBlock1D(nn.Module):
     Parameters:
         in_channels (`int`): The number of channels in the input.
         out_channels (`int`, *optional*, default to be `None`):
-            The number of output channels for the first conv2d layer. If None, same as `in_channels`.
+            The number of output channels for the first conv1d layer. If None, same as `in_channels`.
         dropout (`float`, *optional*, defaults to `0.0`): The dropout probability to use.
-        groups (`int`, *optional*, default to `1`): The number of groups to use for the first normalization layer.
+        temb_channels (`int`, *optional*, default to `512`): the number of channels in timestep embedding.
+        groups (`int`, *optional*, default to `32`): The number of groups to use for the first normalization layer.
         groups_out (`int`, *optional*, default to None):
             The number of groups to use for the second normalization layer. if set to None, same as `groups`.
         eps (`float`, *optional*, defaults to `1e-6`): The epsilon to use for the normalization.
         non_linearity (`str`, *optional*, default to `"swish"`): the activation function to use.
+        time_embedding_norm (`str`, *optional*, default to `"default"` ): Time scale shift config.
+            By default, apply timestep embedding conditioning with a simple shift mechanism. Choose "scale_shift" or
+            "ada_group" for a stronger conditioning with scale and shift.
+        kernel (`torch.FloatTensor`, optional, default to None): FIR filter, see
+            [`~models.resnet.FirUpsample1D`] and [`~models.resnet.FirDownsample1D`].
         output_scale_factor (`float`, *optional*, default to be `1.0`): the scale factor to use for the output.
         use_conv_shortcut (`bool`, default to `True`):
-            If `True`, add a 1x1 nn.conv2d layer for skip-connection.
+            If `True`, add a 1x1 nn.conv1d layer for skip-connection.
         conv_shortcut_bias (`bool`, *optional*, default to `False`):  If `True`, adds a learnable bias to the
             `conv_shortcut` output.
+        up (`bool`, *optional*, default to `False`): If `True`, add an upsample layer.
+        down (`bool`, *optional*, default to `False`): If `True`, add a downsample layer.
         conv_1d_out_channels (`int`, *optional*, default to `None`): the number of channels in the output.
             If None, same as `out_channels`.
-        is_last (`bool`, default to `False`): If `True`, skips a final normalization and activation layer.
     """
 
     def __init__(
-        self,
-        *,
-        in_channels: int,
-        out_channels: Optional[int] = None,
-        dropout: float = 0.0,
-        groups: int = 1,
-        groups_out: Optional[int] = None,
-        eps: float = 1e-6,
-        non_linearity: str = "gelu",
-        output_scale_factor: float = 1.0,
-        use_conv_shortcut: bool = True,
-        conv_shortcut_bias: bool = False,
-        conv_1d_out_channels: Optional[int] = None,
-        is_last: bool = False,
+            self,
+            *,
+            in_channels: int,
+            out_channels: Optional[int] = None,
+            conv_shortcut: bool = False,
+            dropout: float = 0.0,
+            temb_channels: int = 512,
+            groups: int = 32,
+            groups_out: Optional[int] = None,
+            pre_norm: bool = True,
+            eps: float = 1e-6,
+            non_linearity: str = "swish",
+            skip_time_act: bool = False,
+            time_embedding_norm: str = "default",  # default, scale_shift, ada_group, spatial
+            kernel: Optional[torch.FloatTensor] = None,
+            output_scale_factor: float = 1.0,
+            use_conv_shortcut: bool = True,
+            conv_shortcut_bias: bool = False,
+            up: bool = False,
+            down: bool = False,
+            conv_1d_out_channels: Optional[int] = None,
     ):
         super().__init__()
+        self.pre_norm = pre_norm
+        self.pre_norm = True
         self.in_channels = in_channels
         out_channels = in_channels if out_channels is None else out_channels
         self.out_channels = out_channels
+        self.up = up
+        self.down = down
         self.output_scale_factor = output_scale_factor
-        self.is_last = is_last
+        self.time_embedding_norm = time_embedding_norm
+        self.skip_time_act = skip_time_act
 
+        linear_cls = nn.Linear if USE_PEFT_BACKEND else LoRACompatibleLinear
         conv_cls = nn.Conv1d if USE_PEFT_BACKEND else LoRACompatibleConv1d
 
         if groups_out is None:
             groups_out = groups
 
-        self.conv1 = conv_cls(in_channels, out_channels, kernel_size=5, stride=1, padding=2)
-        self.norm1 = torch.nn.GroupNorm(num_groups=groups, num_channels=out_channels, eps=eps, affine=True)
+        if self.time_embedding_norm == "ada_group":
+            self.norm1 = AdaGroupNorm(temb_channels, in_channels, groups, eps=eps)
+        elif self.time_embedding_norm == "spatial":
+            self.norm1 = SpatialNorm(in_channels, temb_channels)
+        else:
+            self.norm1 = torch.nn.GroupNorm(num_groups=groups, num_channels=in_channels, eps=eps, affine=True)
+
+        self.conv1 = conv_cls(in_channels, out_channels, kernel_size=3, stride=1, padding=1)
+
+        if temb_channels is not None:
+            if self.time_embedding_norm == "default":
+                self.time_emb_proj = linear_cls(temb_channels, out_channels)
+            elif self.time_embedding_norm == "scale_shift":
+                self.time_emb_proj = linear_cls(temb_channels, 2 * out_channels)
+            elif self.time_embedding_norm == "ada_group" or self.time_embedding_norm == "spatial":
+                self.time_emb_proj = None
+            else:
+                raise ValueError(f"unknown time_embedding_norm : {self.time_embedding_norm} ")
+        else:
+            self.time_emb_proj = None
+
+        if self.time_embedding_norm == "ada_group":
+            self.norm2 = AdaGroupNorm(temb_channels, out_channels, groups_out, eps=eps)
+        elif self.time_embedding_norm == "spatial":
+            self.norm2 = SpatialNorm(out_channels, temb_channels)
+        else:
+            self.norm2 = torch.nn.GroupNorm(num_groups=groups_out, num_channels=out_channels, eps=eps, affine=True)
+
         self.dropout = torch.nn.Dropout(dropout)
-
         conv_1d_out_channels = conv_1d_out_channels or out_channels
-        self.conv2 = conv_cls(out_channels, conv_1d_out_channels, kernel_size=5, stride=1, padding=2)
+        self.conv2 = conv_cls(out_channels, conv_1d_out_channels, kernel_size=3, stride=1, padding=1)
 
-        self.nonlinearity1 = get_activation(non_linearity)
+        self.nonlinearity = get_activation(non_linearity)
 
-        if not self.is_last:
-            self.group_norm2 = nn.GroupNorm(num_groups=groups_out, num_channels=out_channels, eps=eps, affine=True)
-            self.nonlinearity2 = nn.GELU()
+        self.upsample = self.downsample = None
+        if self.up:
+            if kernel == "fir":
+                fir_kernel = (1, 3, 3, 1)
+                self.upsample = lambda x: upsample_1d(x, kernel=fir_kernel)
+            elif kernel == "sde_vp":
+                self.upsample = partial(F.interpolate, scale_factor=2.0, mode="nearest")
+            else:
+                self.upsample = Upsample1D(in_channels, use_conv=False)
+        elif self.down:
+            if kernel == "fir":
+                fir_kernel = (1, 3, 3, 1)
+                self.downsample = lambda x: downsample_1d(x, kernel=fir_kernel)
+            elif kernel == "sde_vp":
+                self.downsample = partial(F.avg_pool1d, kernel_size=2, stride=2)
+            else:
+                self.downsample = Downsample1D(in_channels, use_conv=False, padding=1, name="op")
 
         self.use_conv_shortcut = (
             self.in_channels != conv_1d_out_channels if use_conv_shortcut is None else use_conv_shortcut
@@ -683,15 +742,69 @@ class ResnetBlock1D(nn.Module):
             )
 
     def forward(self, input_tensor, temb, scale: float = 1.0):
-        hidden_states = self.conv1(input_tensor, scale) if not USE_PEFT_BACKEND else self.conv1(input_tensor)
-        hidden_states = self.norm1(hidden_states)
-        hidden_states = self.nonlinearity1(hidden_states)
+        hidden_states = input_tensor
+
+        if self.time_embedding_norm == "ada_group" or self.time_embedding_norm == "spatial":
+            hidden_states = self.norm1(hidden_states, temb)
+        else:
+            hidden_states = self.norm1(hidden_states)
+
+        hidden_states = self.nonlinearity(hidden_states)
+
+        if self.upsample is not None:
+            # upsample_nearest_nhwc fails with large batch sizes. see https://github.com/huggingface/diffusers/issues/984
+            if hidden_states.shape[0] >= 64:
+                input_tensor = input_tensor.contiguous()
+                hidden_states = hidden_states.contiguous()
+            input_tensor = (
+                self.upsample(input_tensor, scale=scale)
+                if isinstance(self.upsample, Upsample1D)
+                else self.upsample(input_tensor)
+            )
+            hidden_states = (
+                self.upsample(hidden_states, scale=scale)
+                if isinstance(self.upsample, Upsample1D)
+                else self.upsample(hidden_states)
+            )
+        elif self.downsample is not None:
+            input_tensor = (
+                self.downsample(input_tensor, scale=scale)
+                if isinstance(self.downsample, Downsample1D)
+                else self.downsample(input_tensor)
+            )
+            hidden_states = (
+                self.downsample(hidden_states, scale=scale)
+                if isinstance(self.downsample, Downsample1D)
+                else self.downsample(hidden_states)
+            )
+
+        hidden_states = self.conv1(hidden_states, scale) if not USE_PEFT_BACKEND else self.conv1(hidden_states)
+
+        if self.time_emb_proj is not None:
+            if not self.skip_time_act:
+                temb = self.nonlinearity(temb)
+            temb = (
+                self.time_emb_proj(temb, scale)[:, :, None, None]
+                if not USE_PEFT_BACKEND
+                else self.time_emb_proj(temb)[:, :, None, None]
+            )
+
+        if temb is not None and self.time_embedding_norm == "default":
+            hidden_states = hidden_states + temb
+
+        if self.time_embedding_norm == "ada_group" or self.time_embedding_norm == "spatial":
+            hidden_states = self.norm2(hidden_states, temb)
+        else:
+            hidden_states = self.norm2(hidden_states)
+
+        if temb is not None and self.time_embedding_norm == "scale_shift":
+            scale, shift = torch.chunk(temb, 2, dim=1)
+            hidden_states = hidden_states * (1 + scale) + shift
+
+        hidden_states = self.nonlinearity(hidden_states)
+
         hidden_states = self.dropout(hidden_states)
         hidden_states = self.conv2(hidden_states, scale) if not USE_PEFT_BACKEND else self.conv2(hidden_states)
-
-        if not self.is_last:
-            hidden_states = self.group_norm2(hidden_states)
-            hidden_states = self.nonlinearity2(hidden_states)
 
         if self.conv_shortcut is not None:
             input_tensor = (
